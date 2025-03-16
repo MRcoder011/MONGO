7da77ddeb023bdc16c91b8065f3c4fd625f9bcc6
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-16 15:06:07 +0530

    Queue pushing

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+const userSchema = new mongoose.Schema({
+    name : String ,
+    email : String ,
+    age : Number ,
+});
+// collection name is users
+// collection name and model name should be same
+const User  = mongoose.model("User", userSchema);
+User.insertMany([
+    {name : "shafat", email : "123@gmail.com" , age : 24 },
+    {name : "seenu", email : "1234@gmail.com" , age : 25 },
+    {name : "shaffu", email : "12@gmail.comn" , age : 26 },
+]).then((res) => {
+    console.log(res);
+}).catch((error) => {
+    console.log(error);
+});
+/*const user2 = new User({
+    name : "shafat",
+    email : "mdshafat@gmail.com",
+    age : 24,
+
+});
+user2.save().then((res) => {
+    console.log(res);
+}).catch((error) => {
+    console.log(error);
+});
+*/
