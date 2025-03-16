e42944a01536a5018e7e734f753092ebfdff53d3
Md Shafatullah <155663678+MRcoder011@users.noreply.github.com>
2025-03-16 14:06:28 +0530

    Queue pushing

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+
+ 
+main()
+.then(() => { 
+    console.log("Connected to MongoDB");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
